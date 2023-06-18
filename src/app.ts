29b7051e2eb587e85556dc7c4bbd9fb9d6c26fe4
habulCoderJohnny <fokruljohnny@gmail.com>
2023-06-18 23:19:24 +0600

    Implement global error Handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import { ApplicationRoutes } from "./app/routes";
+import globalErrorHandler from "./app/middleware/globalErrorHandler";
 
 const app: Application = express();
 
@@ -16,4 +17,8 @@ app.use("/api/v1/", ApplicationRoutes);
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Digital Cow Hut 🐂🐃");
 });
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
 export default app;
