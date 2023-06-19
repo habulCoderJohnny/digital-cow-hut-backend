6a2efb6c09ea863cca0f30ea19916e27ace77960
habulCoderJohnny <fokruljohnny@gmail.com>
2023-06-19 09:31:13 +0600

    Orders Full Modules Done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { UserRoute } from "../module/users/user.route";
 import { CowRoute } from "../module/cow/cow.route";
+import { OrderRoute } from "../module/orders/order.route";
 
 const router = express.Router();
 
 const moduleRoutes = [
   { path: "/users", route: UserRoute },
   { path: "/cows", route: CowRoute },
+  { path: "/orders", route: OrderRoute },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
